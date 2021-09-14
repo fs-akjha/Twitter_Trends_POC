8764c1532664dbadf0d8137e797cbb0c9ce94db2
Akash Jha <ajha@fleetstudio.com>
2021-09-14 16:20:41 +0530

    first commit

---
+++ Tweet_trends/all_trends_data.py
@@ -0,0 +1,61 @@
+# import tweepy
+# import pandas as pd
+# import os
+
+# #Twitter Access
+# auth = tweepy.OAuthHandler( 'xxx','xxx')
+# auth.set_access_token('xxx-xxx','xxx')
+# api = tweepy.API(auth,wait_on_rate_limit = True)
+
+# df = pd.DataFrame(columns=['text', 'source', 'url'])
+# msgs = []
+# msg =[]
+
+# for tweet in tweepy.Cursor(api.search, q='#bmw', rpp=100).items(10):
+#     msg = [tweet.text, tweet.source, tweet.source_url] 
+#     msg = tuple(msg)                    
+#     msgs.append(msg)
+
+# df = pd.DataFrame(msgs)
+
+
+import tweepy
+import pandas as pd
+import datetime
+import os
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{"host":'localhost',"port":'9200'}])
+
+
+TWITTER_CONSUMER_KEY="ftZimOyZkpVFyKz06ura116Ci"
+TWITTER_CONSUMER_SECRET="eaZUg0MAstAmciLb4tXtI6Xw3ganCEv2KnBRsxPQQuQruyT6SP"
+
+MAX_TWEETS = 50000
+
+auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+api = tweepy.API(auth)
+api = tweepy.API(auth,wait_on_rate_limit = True)
+
+df = pd.DataFrame(columns=['text', 'source', 'url'])
+msgs = []
+msg =[]
+start_date = datetime.datetime(2021, 9, 14, 12, 00, 00)
+end_date = datetime.datetime(2020, 1, 19, 13, 00, 00)
+
+for tweet in tweepy.Cursor(api.search, q='#Ravens', rpp=100).items(MAX_TWEETS):
+    all_data={
+        "id":tweet.user.id,
+        "username": tweet.user.screen_name,
+        "created_at":tweet.user.created_at,
+        "text":tweet.text,
+        "description": tweet.user.description,
+        "location" :tweet.user.location,
+        "following" : tweet.user.friends_count,
+        "followers" : tweet.user.followers_count,
+        "totaltweets" : tweet.user.statuses_count,
+        "retweetcount" : tweet.retweet_count,
+        "hashtags" : tweet.entities['hashtags']
+    }
+    # msgs.append(all_data)
+    es.index(index="trending_hashtags_tweets", doc_type='trendstweets', id=tweet.user.id,body=all_data, request_timeout=200)
+print("Data added")
---
+++ Tweet_trends/trends.py
@@ -0,0 +1,39 @@
+import tweepy
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{"host":'localhost',"port":'9200'}])
+
+# TWITTER_ACCESS_TOKEN="1361217148614402052-sKZR5p2SEe7V8UlrlJePg5STKBAXWA"
+# TWITTER_ACCESS_SECRET="FAWs3H2L26nVkuxVgz3sCJjAUgZk1t2mp3S6AcEomlwpx"
+# TWITTER_CONSUMER_KEY="ftZimOyZkpVFyKz06ura116Ci"
+# TWITTER_CONSUMER_SECRET="eaZUg0MAstAmciLb4tXtI6Xw3ganCEv2KnBRsxPQQuQruyT6SP"
+
+# assign the values accordingly
+consumer_key = "ftZimOyZkpVFyKz06ura116Ci"
+consumer_secret = "eaZUg0MAstAmciLb4tXtI6Xw3ganCEv2KnBRsxPQQuQruyT6SP"
+access_token = "1361217148614402052-sKZR5p2SEe7V8UlrlJePg5STKBAXWA"
+access_token_secret = "FAWs3H2L26nVkuxVgz3sCJjAUgZk1t2mp3S6AcEomlwpx"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  
+# set access to user's access key and access secret 
+auth.set_access_token(access_token, access_token_secret)
+  
+# calling the api 
+api = tweepy.API(auth)
+  
+# fetching the trends
+trends = api.trends_place(1)
+  
+# printing the information
+print("The top trends for the location are :")
+  
+for value in trends:
+    for trend in value['trends']:
+        data={
+            'name':trend['name'],
+            'url':trend['url'],
+            'query':trend['query']
+        }
+        es.index(index="trending_hashtags", doc_type='trends', body=data, request_timeout=200)
+
+print("Trending Hashtags Added")
